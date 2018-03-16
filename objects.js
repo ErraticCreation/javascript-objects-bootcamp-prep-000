8715c5c9670588dd44c9c87776b2d4daa85f73b5
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 22:34:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,8 @@
 var playlist = { Slipknot: "Wait and Bleed" };
 
 var updatePlaylist = function(playlist, artistName, songTitle){
-  
+  playlist[artistName] = songTitle;
+  return playlist;
 }
+
+console.log(updatePlaylist(playlist, "KoRn", "Alone I Break"));
